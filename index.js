5bc52ab065a9d092e9f5b2760300f0c52904b640
Dami <ezio18@o2.pl>
2024-02-22 08:47:16 +0100

    AC

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 let aString = "";
 let bString = "";
-let ekran = document.querySelector(".ekran");
+let ekran = document.querySelector("#number");
 let buttons = document.querySelectorAll(
   "button[id='0'], button[id='1'], button[id='2'], button[id='3'], button[id='4'], button[id='5'], button[id='6'], button[id='7'], button[id='8'], button[id='9']"
 );
-//if id.target.innerText.length < 10 , aString += id.target.innerText;
+
+let actions = document.querySelectorAll(
+  "button[id='AC'], button[id='plusMinus'], button[id='procent'], button[id='div'], button[id='multi'], button[id='subs'], button[id='add'], button[id='dot'], button[id='result']"
+);
+
 buttons.forEach((button) => {
   button.addEventListener("click", (id) => {
-    aString += id.target.innerText;
-    console.log(aString);
+    if (aString.length < 10) {
+      aString += id.target.innerText;
+    }
     ekran.innerText = aString;
   });
 });
 
-let a = 0;
-let b = 0;
+actions.forEach((button) => {
+  button.addEventListener("click", (id) => {
+    if (id.target.id == "AC") {
+      aString = "";
+      ekran.innerText = "0";
+    }
+  });
+});
+
 function add(a, b) {
   return a + b;
 }
